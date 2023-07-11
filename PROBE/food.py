c0b22b7774202945ed6f2567f21be35e322c4947
KohPym <138464026+KohPym@users.noreply.github.com>
2023-07-11 19:12:05 +0200

    Update food.py

--- PROBE/food.py
+++ PROBE/food.py
@@ -3,99 +3,109 @@
 apple = {
     'name': 'Apple',
     'satiety': 8,
+    'vitamins': 2,
     'hydration': 5,
     'toxicity': 0,
     'taste': 'sweet',
-    'vitamins': ['A', 'C'],
-    'texture': 'crispy'
-}
-
-cactus = {
-    'name': 'Cactus',
-    'satiety': 3,
-    'hydration': 10,
-    'toxicity': 1,
-    'taste': 'bitter',
-    'vitamins': ['C', 'E'],
-    'texture': 'prickly'
-}
-
-water = {
-    'name': 'Water',
-    'satiety': 0,
-    'hydration': 10,
-    'toxicity': 0,
-    'taste': 'neutral',
-    'vitamins': [],
-    'texture': 'liquid'
+    'texture': 'crunchy',
+    'color': 'red'
 }
 
 salmon = {
     'name': 'Salmon',
     'satiety': 7,
+    'vitamins': 2,
     'hydration': 2,
     'toxicity': 0,
     'taste': 'savory',
-    'vitamins': ['B12', 'D'],
-    'texture': 'oily'
+    'texture': 'oily',
+    'color': 'pink'
 }
 
 mushroom = {
     'name': 'Mushroom',
     'satiety': 5,
+    'vitamins': 2,
     'hydration': 1,
     'toxicity': 5,
     'taste': 'earthy',
-    'vitamins': ['B2', 'D'],
-    'texture': 'spongy'
+    'texture': 'spongy',
+    'color': 'brown'
 }
 
 blueberry = {
     'name': 'Blueberry',
     'satiety': 4,
+    'vitamins': 2,
     'hydration': 6,
     'toxicity': 0,
     'taste': 'sweet',
-    'vitamins': ['C', 'K'],
-    'texture': 'juicy'
+    'texture': 'juicy',
+    'color': 'blue'
 }
 
 elderberry = {
     'name': 'Elderberry',
-    'satiety': 1,
+    'satiety': 3,
+    'vitamins': 2,
     'hydration': 6,
-    'toxicity': 6,
-    'taste': 'sweet',
-    'vitamins': ['C', 'K'],
-    'texture': 'juicy'
-}
-
-coconut_water = {
-    'name': 'Coconut Water',
-    'satiety': 1,
-    'hydration': 9,
-    'toxicity': 0,
-    'taste': 'sweet',
-    'vitamins': ['C', 'B6'],
-    'texture': 'refreshing'
+    'toxicity': 3,
+    'taste': 'tart',
+    'texture': 'juicy',
+    'color': 'purple'
 }
 
 potato = {
     'name': 'Potato',
     'satiety': 6,
+    'vitamins': 1,
     'hydration': 3,
     'toxicity': 0,
     'taste': 'starchy',
-    'vitamins': ['B6', 'C'],
-    'texture': 'soft'
+    'texture': 'firm',
+    'color': 'brown'
+}
+
+cactus = {
+    'name': 'Cactus',
+    'satiety': 3,
+    'vitamins': 1,
+    'hydration': 10,
+    'toxicity': 1,
+    'taste': 'bitter',
+    'texture': 'prickly',
+    'color': 'green'
+}
+
+water = {
+    'name': 'Water',
+    'satiety': 0,
+    'vitamins': 0,
+    'hydration': 10,
+    'toxicity': 0,
+    'taste': 'neutral',
+    'texture': 'liquid',
+    'color': 'clear'
+}
+
+coconut_water = {
+    'name': 'Coconut Water',
+    'satiety': 1,
+    'vitamins': 2,
+    'hydration': 9,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'texture': 'refreshing',
+    'color': 'clear'
 }
 
 maple_syrup = {
     'name': 'Maple Syrup',
     'satiety': 1,
+    'vitamins': 0,
     'hydration': 5,
     'toxicity': 0,
     'taste': 'sweet',
-    'vitamins': [],
-    'texture': 'sticky'
+    'texture': 'sticky',
+    'color': 'brown'
 }

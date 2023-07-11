993f16133e4adee2ef10a2d740480c86a15bab8a
KohPym <138464026+KohPym@users.noreply.github.com>
2023-07-11 16:00:47 +0200

    Update food.py

--- PROBE/food.py
+++ PROBE/food.py
@@ -1,11 +1,103 @@
-class Food:
-    def __init__(self,*args):
-        self.args = args
 
-class Apple(Food):
-    def __init__(self):
-        super().__init__("Apple", 10, 5, 0)
 
-class Cactus(Food):
-    def __init__(self):
-        super().__init__("Cactus", 2, 8, 2)
+# food.py
+
+apple = {
+    'name': 'Apple',
+    'satiety': 8,
+    'hydration': 5,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'vitamins': ['A', 'C'],
+    'texture': 'crispy'
+}
+
+cactus = {
+    'name': 'Cactus',
+    'satiety': 3,
+    'hydration': 10,
+    'toxicity': 1,
+    'taste': 'bitter',
+    'vitamins': ['C', 'E'],
+    'texture': 'prickly'
+}
+
+water = {
+    'name': 'Water',
+    'satiety': 0,
+    'hydration': 10,
+    'toxicity': 0,
+    'taste': 'neutral',
+    'vitamins': [],
+    'texture': 'liquid'
+}
+
+salmon = {
+    'name': 'Salmon',
+    'satiety': 7,
+    'hydration': 2,
+    'toxicity': 0,
+    'taste': 'savory',
+    'vitamins': ['B12', 'D'],
+    'texture': 'oily'
+}
+
+mushroom = {
+    'name': 'Mushroom',
+    'satiety': 5,
+    'hydration': 1,
+    'toxicity': 5,
+    'taste': 'earthy',
+    'vitamins': ['B2', 'D'],
+    'texture': 'spongy'
+}
+
+blueberry = {
+    'name': 'Blueberry',
+    'satiety': 4,
+    'hydration': 6,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'vitamins': ['C', 'K'],
+    'texture': 'juicy'
+}
+
+orange_juice = {
+    'name': 'Orange Juice',
+    'satiety': 2,
+    'hydration': 8,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'vitamins': ['C'],
+    'texture': 'liquid'
+}
+
+coconut_water = {
+    'name': 'Coconut Water',
+    'satiety': 1,
+    'hydration': 9,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'vitamins': ['C', 'B6'],
+    'texture': 'refreshing'
+}
+
+tomato_juice = {
+    'name': 'Tomato Juice',
+    'satiety': 2,
+    'hydration': 8,
+    'toxicity': 0,
+    'taste': 'salty',
+    'vitamins': ['A', 'C'],
+    'texture': 'liquid'
+}
+
+maple_syrup = {
+    'name': 'Maple Syrup',
+    'satiety': 1,
+    'hydration': 5,
+    'toxicity': 0,
+    'taste': 'sweet',
+    'vitamins': [],
+    'texture': 'sticky'
+}

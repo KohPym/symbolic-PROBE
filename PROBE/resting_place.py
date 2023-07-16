b9a2858816860a4040877c568f6a78956ff40f6e
KohPym <138464026+KohPym@users.noreply.github.com>
2023-07-16 10:34:06 +0200

    Update and rename l to resting_place.py

---
+++ PROBE/resting_place.py
@@ -0,0 +1,43 @@
+# resting_places.py
+
+rock = {
+    'name': 'Rock',
+    'type': 'resting place',
+    'terrain': 'smooth surface',
+    'comfort': 2,
+    'noise_level': 1
+}
+
+log = {
+    'name': 'Log',
+    'type': 'resting place',
+    'terrain': 'sturdy seat',
+    'comfort': 2,
+    'noise_level': 1
+}
+
+leaf_pile = {
+    'name': 'Leaf Pile',
+    'type': 'resting place',
+    'terrain': 'soft bed',
+    'comfort': 3,
+    'noise_level': 2
+}
+
+mossy_bank = {
+    'name': 'Mossy Bank',
+    'type': 'resting place',
+    'terrain': 'soft surface',
+    'comfort': 4,
+    'noise_level': 2
+}
+
+sand = {
+    'name': 'Sand',
+    'type': 'resting place',
+    'terrain': 'cozy bed',
+    'comfort': 3,
+    'noise_level': 1
+}
+
+resting_places = [rock, log, leaf_pile, mossy_bank, sand]

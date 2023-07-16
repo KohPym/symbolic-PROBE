0da824bb094d7c3e879e5ec8366d8697042b5577
KohPym <138464026+KohPym@users.noreply.github.com>
2023-07-16 18:14:49 +0200

    Create food.py

---
+++ PROBE/food.py
@@ -0,0 +1,141 @@
+import random
+
+apple = {
+    'name': 'Apple',
+    'type': 'food',
+    'satiety': 8,
+    'vitamins': 2,
+    'hydration': 5,
+    'toxicity': 0,
+    'humidity': 'moderate',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(0, 1000),
+    'oxygen': 'moderate'
+}
+
+mushroom = {
+    'name': 'Mushroom',
+    'type': 'food',
+    'satiety': 5,
+    'vitamins': 2,
+    'hydration': 1,
+    'toxicity': 5,
+    'humidity': 'wet',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(-1000, 2000),
+    'oxygen': 'moderate'
+}
+
+blueberry = {
+    'name': 'Blueberry',
+    'type': 'food',
+    'satiety': 4,
+    'vitamins': 2,
+    'hydration': 6,
+    'toxicity': 0,
+    'humidity': 'moderate',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(0, 500),
+    'oxygen': 'moderate'
+}
+
+elderberry = {
+    'name': 'Elderberry',
+    'type': 'food',
+    'satiety': 3,
+    'vitamins': 2,
+    'hydration': 6,
+    'toxicity': 3,
+    'humidity': 'moderate',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(0, 500),
+    'oxygen': 'moderate'
+}
+
+potato = {
+    'name': 'Potato',
+    'type': 'food',
+    'satiety': 6,
+    'vitamins': 1,
+    'hydration': 3,
+    'toxicity': 0,
+    'humidity': 'moderate',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(0, 300),
+    'oxygen': 'moderate'
+}
+
+salmon = {
+    'name': 'Salmon',
+    'type': 'food',
+    'satiety': 7,
+    'vitamins': 2,
+    'hydration': 2,
+    'toxicity': 0,
+    'humidity': 'wet',
+    'vegetation': False,
+    'water': True,
+    'altitude': random.randint(-1000, 300),
+    'oxygen': 'low'
+}
+
+cactus = {
+    'name': 'Cactus',
+    'type': 'food',
+    'satiety': 3,
+    'vitamins': 1,
+    'hydration': 10,
+    'toxicity': 1,
+    'humidity': 'dry',
+    'vegetation': False,
+    'water': False,
+    'altitude': random.randint(0, 300),
+    'oxygen': 'low'
+}
+
+water = {
+    'name': 'Water',
+    'type': 'liquid',
+    'satiety': 0,
+    'vitamins': 0,
+    'hydration': 10,
+    'toxicity': 0,
+    'humidity': 'wet',
+    'vegetation': False,
+    'water': True,
+    'altitude': random.randint(-2000, 1000),
+    'oxygen': 'moderate'
+}
+
+coconut_water = {
+    'name': 'Coconut Water',
+    'type': 'liquid',
+    'satiety': 1,
+    'vitamins': 2,
+    'hydration': 9,
+    'toxicity': 0,
+    'humidity': 'wet',
+    'vegetation': True,
+    'water': True,
+    'altitude': random.randint(0, 1000),
+    'oxygen': 'high'
+}
+
+maple_syrup = {
+    'name': 'Maple Syrup',
+    'type': 'liquid',
+    'satiety': 1,
+    'vitamins': 0,
+    'hydration': 5,
+    'toxicity': 0,
+    'humidity': 'wet',
+    'vegetation': False,
+    'water': True,
+    'altitude': random.randint(0,800)
+    'oxygen': 'moderate'
+}

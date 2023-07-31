7266cec717a57574ecfa2091fe3ea6bb06a74392
KohPym <138464026+KohPym@users.noreply.github.com>
2023-07-31 22:52:27 +0200

    Update counting_matrix.py [FINISHED]

--- PROBE/counting_matrix.py
+++ PROBE/counting_matrix.py
@@ -28,23 +28,3 @@ class CountingMatrix:
         to_state = self.state_dict[to_state_name]
         self.count_mat[from_state][to_state] += 1
         return self.count_mat
-
-
-
-# Créer la matrice de comptage et le dictionnaire de correspondance entre les noms d'états et leurs indices
-count_mat, state_dict = counting_matrix()
-
-# Effectuer une transition de l'état "Consume" à l'état "Flee"
-count_mat = update_counting_matrix(count_mat, state_dict, "Consume", "Flee")
-
-# Effectuer une transition de l'état "Flee" à l'état "Random"
-count_mat = update_counting_matrix(count_mat, state_dict, "Flee", "Random")
-
-# Afficher la matrice de comptage mise à jour
-print(count_mat)
-
-[[0 1 0 0 0]
- [0 0 0 0 1]
- [0 0 0 0 0]
- [0 0 0 0 0]
- [0 0 0 0 0]]

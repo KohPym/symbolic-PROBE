38e4e7e4bbbd65ba473f7769e2753b13ef43642e
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-02 11:05:59 +0200

    Update decoder.py

--- PROBE/decoder.py
+++ PROBE/decoder.py
@@ -1,24 +1,21 @@
-class Decoder:
-    def __init__(self):
-        self.mapping_str_to_int = {
+def decode(arg):
+    if isinstance(arg, str):
+        mapping = {
             "Consume": 1,
             "Flee": 2,
             "Random": 3,
             "Rest": 4,
             "Stock": 5
         }
-        self.mapping_int_to_str = {
+        return mapping.get(arg, "Invalid input: string not recognized")
+    elif isinstance(arg, int):
+        mapping = {
             1: "Consume",
             2: "Flee",
             3: "Random",
             4: "Rest",
             5: "Stock"
         }
-
-    def decode(self, arg):
-        if isinstance(arg, str):
-            return self.mapping_str_to_int.get(arg, "Invalid input: string not recognized")
-        elif isinstance(arg, int):
-            return self.mapping_int_to_str.get(arg, "Invalid input: integer not recognized")
-        else:
-            return "Invalid input: argument must be a string or an integer"
+        return mapping.get(arg, "Invalid input: integer not recognized")
+    else:
+        return "Invalid input: argument must be a string or an integer"

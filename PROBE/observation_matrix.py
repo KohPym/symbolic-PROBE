573fc113c77eb19e1cae29cdc0e1e6d0a38ec8a7
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-09 13:03:23 +0200

    Update observation_matrix.py

--- PROBE/observation_matrix.py
+++ PROBE/observation_matrix.py
@@ -8,33 +8,17 @@ import numpy as np
 class ObservationMatrix:
     def __init__(self):
         self.matrix = np.zeros((10, 6))
+        self.norm_matrix = np.zeros((10, 6))
 
-    def update(self, current_biome, values): #Pense à ajouter que 4 valeurs sur 6
-        biome_mapping = {
-            "Rainforest": 0,
-            "Desert": 1,
-            "Tundra": 2,
-            "Plains": 3,
-            "Savanna": 4,
-            "Mangrove": 5,
-            "Plains": 6,
-            "Swamp": 7,
-            "Taiga": 8,
-            "Beach": 9,
-        }
-      
-        if current_biome in biome_mapping:
-            line = biome_mapping[current_biome]
-            num_values = len(values)
-            self.matrix[line, :num_values] += values
+    def update(self, current_biome, values):
+        num_values = len(values)
+        self.matrix[current_biome, :num_values] += values
+        self.matrix[current_biome, 5] += 1
 
     def normalize_matrix(self):
-        norm_matrix = self.matrix
-        matrix_sum = np.sum(norm_matrix)
-        if matrix_sum != 0:
-            norm_matrix /= matrix_sum
+        self.norm_matrix = self.matrix
+        row_sums = np.sum(self.matrix[:, :4], axis=1, keepdims=True)
+        for i in range(row_sums.shape[0]):
+            if row_sums[i] != 0:
+                self.norm_matrix[i, :4] = np.round(self.norm_matrix[i, :4] / row_sums[i], 2)
 
-
-    def normalize_matrix(self):
-        # Normalise les 4 premiers termes de chaque ligne de la matrice
-        self.matrix[:, :4] /= np.sum(self.matrix[:, :4], axis=1, keepdims=True)

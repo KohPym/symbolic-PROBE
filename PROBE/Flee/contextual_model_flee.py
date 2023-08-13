f61ce1a55728848a75cc472ad17e4e2667032044
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-13 21:25:30 +0200

    Update contextual_model_flee.py

--- PROBE/Flee/contextual_model_flee.py
+++ PROBE/Flee/contextual_model_flee.py
@@ -23,6 +23,27 @@ class AnteReliability_Flee:
         # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
         # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
         # We then apply a decision structure to introduce the interoception mechanism.
+        self.observation = observation  # Somme et moeynne pour conso etc
+        self.contextual_mapping = self.observation
+
+    def update_contextual(self, biome, mu, learning_rate):  # Update du contexte F
+        self.contextual_mapping[biome] = learning_rate * mu + (1 - learning_rate) * self.contextual_mapping[biome] # Voué a converger
+        return self.contextual_mapping
+
+    def update_lambda(self, lambda_flee, biome, tau, mu):
+        lambda_flee[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu))  # NORMALISE
+        return lambda_flee
+
+
+ante = AnteReliability_Flee([1,2,3,4,5,6,7,8,9,10])
+ante.update_contextual(7, 1, 0.01)
+ante.update_lambda([1,1,1,1,1,1,1,1,1,1], 6, [1,2,3], [2,3,4])
+
+class AnteReliability_Flee:
+    def __init__(self, observation):
+        # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
+        # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
+        # We then apply a decision structure to introduce the interoception mechanism.
         self.observation = observation # Somme et moeynne pour conso etc
         self.contextual_mapping = self.observation
         

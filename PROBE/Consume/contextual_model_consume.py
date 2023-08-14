ea90219df49c000964e82c5bce667c5b65d1f19f
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-15 01:27:26 +0200

    Create contextual_model_consume.py

---
+++ PROBE/Consume/contextual_model_consume.py
@@ -0,0 +1,15 @@
+class AnteReliability_Consume:
+    def __init__(self, observation):
+        # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
+        # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
+        # We then apply a decision structure to introduce the interoception mechanism.
+        self.observation = observation  # Somme et moeynne pour conso etc
+        self.contextual_mapping = self.observation
+
+    def update_contextual(self, biome, mu, learning_rate):  # Update du contexte F
+        self.contextual_mapping[biome] = learning_rate * mu + (1 - learning_rate) * self.contextual_mapping[biome] # Voué a converger
+        return self.contextual_mapping
+
+    def update_lambda(self, lambda_consume, biome, tau, mu):
+        lambda_consume[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu))  # NORMALISE
+        return lambda_consume

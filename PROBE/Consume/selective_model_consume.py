316e84f89ccdc2be7d40831f20b81dabd85e84b7
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-16 01:58:23 +0200

    Create selective_model_consume.py

---
+++ PROBE/Consume/selective_model_consume.py
@@ -0,0 +1,20 @@
+from SymbolicEnv import SymbolicEnv
+
+class SelectiveModel_Consume:
+    def __init__(self):
+        self.env = SymbolicEnv()
+        check_env(env)
+
+    def train_qlearning(self, total_timesteps, learning_rate, discount_factor, log_name="runs/ql"):
+        model = QLearning(self.env)
+        model.learn(total_timesteps, learning_rate, discount_factor, log_name)
+
+    def train_symbolic_qlearning(self, total_timesteps, learning_rate, discount_factor, radius, log_name=f"runs/symbolic-ql-{r}"):
+        radii = np.linspace(0.01, 0.25, 10)
+        for r in radii:
+            model = SymbolicQLearning(self.env, self.env.dist)
+            model.learn(total_timesteps, learning_rate, discount_factor, radius, log_name)
+          
+    def action_to_choose(self):
+      return np.argmax(model.qtable[tuple(observation)])
+         

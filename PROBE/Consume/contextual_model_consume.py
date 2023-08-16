a357dda7b3de3c64a8523809b5687819d65f42de
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-16 14:40:02 +0200

    Update contextual_model_consume.py

--- PROBE/Consume/contextual_model_consume.py
+++ PROBE/Consume/contextual_model_consume.py
@@ -1,15 +1,18 @@
+import numpy as np
+
 class AnteReliability_Consume:
     def __init__(self, observation):
-        # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
-        # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
-        # We then apply a decision structure to introduce the interoception mechanism.
-        self.observation = observation  # Somme et moeynne pour conso etc
+        # Initially, the AnteReliability_Consume is equal to the contextual mapping due to the design of our experiment.
+        # Others parameters are initially equal to 0, and we have an observation of our environment which serve as
+        # contextual_mapping.
+        self.observation = observation
         self.contextual_mapping = self.observation
 
-    def update_contextual(self, biome, mu, learning_rate):  # Update du contexte F
-        self.contextual_mapping[biome] = learning_rate * mu + (1 - learning_rate) * self.contextual_mapping[biome] # Voué a converger
+    def update_contextual(self, biome, mu_consume, learning_rate):
+        self.contextual_mapping[biome] = learning_rate * mu_consume + \
+                                         (1 - learning_rate) * self.contextual_mapping[biome]
         return self.contextual_mapping
 
-    def update_lambda(self, lambda_consume, biome, tau, mu):
-        lambda_consume[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu))  # NORMALISE
+    def update_lambda(self, lambda_consume, biome, tau, mu_consume):
+        lambda_consume[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu_consume))  # NORMALISE
         return lambda_consume

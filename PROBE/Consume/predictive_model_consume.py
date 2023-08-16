23ce2819b8632ca28090f8ea44791b292d3c47ea
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-16 14:40:21 +0200

    Update predictive_model_consume.py

--- PROBE/Consume/predictive_model_consume.py
+++ PROBE/Consume/predictive_model_consume.py
@@ -1,15 +1,12 @@
 class PostReliability_Consume:
     def __init__(self, observation):
-        # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
-        # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
-        # We then apply a decision structure to introduce the interoception mechanism.
-        self.observation = observation  # Somme et moeynne pour conso etc
+        self.observation = observation
         self.predictive_mapping = self.observation
 
-    def update_predictive(self, biome, lambda_consume, learning_rate):  # Update du contexte F
-        self.predictive_mapping[biome] = learning_rate * mu + (1 - learning_rate) * self.predictive_mapping[biome] # Voué a converger
+    def update_predictive(self, biome, homeostasis_score, action_probability, t):
+        self.predictive_mapping[biome] = (homeostasis_score[t] / homeostasis_score[t-1]) * action_probability
         return self.predictive_mapping
 
     def update_mu(self, lambda_consume, biome, mu_consume):
-        mu_consume[biome] = self.predictive_mapping[biome] * lambda_consume  # NORMALISE
+        mu_consume[biome] = self.predictive_mapping[biome] * lambda_consume
         return mu_consume

39d395c2e4590f6aa65d6298bf67d039aa94c776
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-16 14:41:06 +0200

    Update predictive_model_flee.py

--- PROBE/Flee/predictive_model_flee.py
+++ PROBE/Flee/predictive_model_flee.py
@@ -1,1 +1,12 @@
+class PostReliability_Flee:
+    def __init__(self, observation):
+        self.observation = observation
+        self.predictive_mapping = self.observation
 
+    def update_predictive(self, biome, homeostasis_score, action_probability, t):
+        self.predictive_mapping[biome] = (homeostasis_score[t] / homeostasis_score[t-1]) * action_probability
+        return self.predictive_mapping
+
+    def update_mu(self, lambda_flee, biome, mu_flee):
+        mu_flee[biome] = self.predictive_mapping[biome] * lambda_flee
+        return mu_flee

eef1f288aa6a71b290ab29abea482d86a21ba311
KohPym <138464026+KohPym@users.noreply.github.com>
2023-08-16 14:41:22 +0200

    Update contextual_model_random.py

--- PROBE/Random/contextual_model_random.py
+++ PROBE/Random/contextual_model_random.py
@@ -1,15 +1,18 @@
+import numpy as np
+
 class AnteReliability_Random:
     def __init__(self, observation):
-        # Initially, the AnteReliability_Flee is equal to the contextual mapping due to the design of our experiment.
-        # Others parameters are intially equal to 0 and we have an observation of our environment which serve as contextual_mapping.
-        # We then apply a decision structure to introduce the interoception mechanism.
-        self.observation = observation  # Somme et moeynne pour conso etc
+        # Initially, the AnteReliability_Random is equal to the contextual mapping due to the design of our
+        # experiment. Others parameters are initially equal to 0, and we have an observation of our environment which
+        # serve as contextual_mapping.
+        self.observation = observation
         self.contextual_mapping = self.observation
 
-    def update_contextual(self, biome, mu, learning_rate):  # Update du contexte F
-        self.contextual_mapping[biome] = learning_rate * mu + (1 - learning_rate) * self.contextual_mapping[biome] # Voué a converger
+    def update_contextual(self, biome, mu_random, learning_rate):
+        self.contextual_mapping[biome] = learning_rate * mu_random + \
+                                         (1 - learning_rate) * self.contextual_mapping[biome]
         return self.contextual_mapping
 
-    def update_lambda(self, lambda_random, biome, tau, mu):
-        lambda_random[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu))  # NORMALISE
+    def update_lambda(self, lambda_random, biome, tau, mu_random):
+        lambda_random[biome] = self.contextual_mapping[biome] * np.sum(np.multiply(tau, mu_random))
         return lambda_random
